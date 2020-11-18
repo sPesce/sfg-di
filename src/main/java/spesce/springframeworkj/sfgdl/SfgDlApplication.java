11acbf217c3a01fc9caa80bf72d62d9a5a2cad82
Steve <stevepesce879@gmail.com>
2020-11-18 14:44:07 -0500

    added spring starter

---
+++ src/main/java/spesce/springframeworkj/sfgdl/SfgDlApplication.java
@@ -0,0 +1,13 @@
+package spesce.springframeworkj.sfgdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spesce/springframeworkj/sfgdl/SfgDlApplicationTests.java
@@ -0,0 +1,13 @@
+package spesce.springframeworkj.sfgdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
