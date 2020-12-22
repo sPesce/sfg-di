7099101adab36d0bcf24ee7a70551c5363112005
Steve <stevepesce879@gmail.com>
2020-12-22 16:39:22 -0500

    refactored package names

---
+++ src/main/java/guru/services/GreetingRepository.java
@@ -0,0 +1,13 @@
+package guru.services;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
---
+++ src/main/java/guru/services/GreetingService.java
@@ -0,0 +1,9 @@
+package guru.services;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public interface GreetingService {
+
+    String sayGreeting();
+}
